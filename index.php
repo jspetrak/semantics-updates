ad8c8b807b89f94d3c7a30e64b3383cc2e82cdb6
Josef Petrák <me@jspetrak.name>
2011-03-16 23:10:01 +0100

    Basic XHTML markup on index

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+<title>What's new | Semanti–CS Updates</title>
+<link href="screen.css" type="text/css" rel="stylesheet" media="screen" />
+</head>
+<body>
+
 <?php
 
 	require_once('_library.php');
@@ -29,3 +39,5 @@ ORDER BY DESC(?date)
 	}
 
 ?>
+</body>
+</html>
